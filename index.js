e41fdb786b82584974cb3ea7ff2d780152024c19
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-05-22 21:05:29 -0700

    constuctor added to index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require('./lib/shapes');
+const shapes = require('./lib/shapes');
 
 const questions = [
     {
@@ -10,7 +10,7 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What color do you want the text to be',
+        message: 'What color do you want the text to be (OR a hexadecimal number)',
         name: 'text-color',
     },
     {
@@ -21,7 +21,37 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What color do you want the logo to be',
+        message: 'What color do you want the logo to be (OR a hexadecimal number)',
         name: 'logo-color',
     }
 ];
+
+class Svg {
+    constructor() {
+        this.textElement = '';
+        this.shapeElement = '';
+    }
+    render() {
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`;
+    }
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+    }
+    setShapeElement(shape) {
+        this.shapeElement = shape.render();
+    }
+}
+
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Logo created successfully!");
+    });
+}
+
+
+
+init();
