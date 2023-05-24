a3c13f8244117a105f783f8444899b6665aaa1a6
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-05-23 20:51:51 -0700

    added function to initialize app

--- index.js
+++ index.js
@@ -52,6 +52,40 @@ const writeToFile = (fileName, data) => {
     });
 }
 
+const init = async () => {
+    const answers = await inquirer.prompt(questions);
+    const { text, 'text-color': textColor, 'logo-color': logoColor, 'logo shape': logoShape } = answers;
 
+    if (!(text.length > 0 && text.length < 4)) {
+        console.log("Invalid. Please enter up to three characters");
+        return;
+    }
+
+    let shape;
+    switch (logoShape) {
+        case 'Circle':
+            shape = new shapes.Circle();
+            break;
+        case 'Square':
+            shape = new shapes.Square();
+            break;
+        case 'Triangle':
+            shape = new shapes.Triangle();
+            break;
+        default:
+            console.log("Invalid shape!");
+            return;
+    }
+
+    shape.setColor(logoColor);
+
+    const svg = new Svg();
+    svg.setTextElement(text, textColor);
+    svg.setShapeElement(shape);
+    const svgString = svg.render();
+
+    console.log(`Created logo: \n${svgString}`);
+    writeToFile("logo.svg", svgString);
+}
 
 init();
